98dfdc7b2d68b6f412156cff4b7d96a6b52236b2
Sergio <sgbenav@gmail.com>
2021-07-31 15:39:15 -0600

    add prettier :shipit:

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,38 +1,45 @@
 import { createClient } from 'contentful'
+import Image from 'next/image'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_KEY
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 })
 
-export default function RecipeDetails({post}) {
+export default function RecipeDetails({ post }) {
+  const { featureImage, title, content } = post.fields
   return (
     <div>
-      Recipe Details
+      <div>
+        <Image
+          src={`https: ${featureImage.fields.file.url}`}
+          width={featureImage.fields.file.details.image.width}
+          height={featureImage.fields.file.details.image.height}
+        />
+      </div>
     </div>
   )
 }
 
 export async function getStaticPaths() {
   const res = await client.getEntries({
-    content_type: 'post'
+    content_type: 'post',
   })
 
-  const paths = res.items.map(item => ({
-    params: {slug: item.fields.slug}
+  const paths = res.items.map((item) => ({
+    params: { slug: item.fields.slug },
   }))
 
   return { paths, fallback: false }
 }
 
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
   const { items } = await client.getEntries({
     content_type: 'post',
-    'fields.slug': params.slug
+    'fields.slug': params.slug,
   })
 
   return {
-    props: { post: items[0]}
+    props: { post: items[0] },
   }
-
 }
